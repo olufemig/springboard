e869db7dde2ea9132b64b673b818ebd01465f896
Olufemi George <olufemig@outlook.com>
2017-03-25 13:13:03 +0000

    first titanic submission

--- DW2.R
+++ DW2.R
@@ -1,3 +1,16 @@
 library(dplyr)
 library(tidyr)
-df.titanic <- read.csv("titanic3.csv")
+library(Amelia)
+df.titanic <- read.csv("titanic3.csv",,na.strings=c("","na"))
+summary(df.titanic$embarked)
+# lookup all missing values in embarked column
+df.titanic[is.na(df.titanic$embarked),]
+df<-df.titanic
+#convert to string in order for next stage not fail
+df$embarked <- lapply(df$embarked,as.character)
+#update missing fields with S
+df$embarked[which(is.na(df$embarked))]<- "S"
+df2<-df
+#check if there are any nulls left
+df2.titanic[is.na(df.titanic$embarked),]
+
