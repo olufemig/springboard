f01d649fb4febca713595b3fbba26fa430f23ad1
Olufemi George <olufemig@outlook.com>
2017-03-28 20:53:09 +0100

    first titanic mini project draft

---
+++ titanic.R
@@ -0,0 +1,17 @@
+# titanic is avaliable in your workspace
+str(titanic)
+# Check out the structure of titanic
+summary(titanic)
+# Use ggplot() for the first instruction
+ggplot(titanic, aes(x=factor(Pclass),fill=factor(Sex))) +
+  geom_bar(position= "dodge") +
+  geom_line
+# Use ggplot() for the second instruction
+
+
+# Position jitter (use below)
+posn.j <- position_jitter(0.5, 0)
+
+# Use ggplot() for the last instruction
+
+
